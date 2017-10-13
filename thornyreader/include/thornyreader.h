f600eabff4491062ac010529bdbb1f4a32b5a706
Dmitry Razdrokin <d.razdrokin@gmail.com>
2017-10-13 13:38:08 +0300

    Version 17.07.31.00

--- thornyreader/include/thornyreader.h
+++ thornyreader/include/thornyreader.h
@@ -17,7 +17,7 @@
 #ifndef _THORNYREADER_H_
 #define _THORNYREADER_H_
 
-#define THORNYREADER_VERSION "17.07.31.00"
+#define THORNYREADER_VERSION "17.10.13.00"
 #define THORNYREADER_LOG_TAG "thornyreader"
 
 const bool ThornyReaderIsDebugBuild();
